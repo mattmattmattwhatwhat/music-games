bf0feb1017911fa05ff8717239c8ddd26346f6e5
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2014-07-07 16:08:49 -0400

    added Song class, test importing tab delimited song list

--- get_spotify_ids.py
+++ get_spotify_ids.py
@@ -1,5 +1,24 @@
 import requests
 
+# I don't have a big song database and won't be able to github.io host a
+# spotify API key, so this will be a fragile way to get spotify ids in
+# usable form
+
+class Song:
+	def __init__(self, artist, song_title):
+		self.artist = artist
+		self.title = song_title
+
+song_set = []
+
 with open('song_list.txt') as f:
 	for line in f:
-		print line.strip().split('\t')
+		# lines in the form: year \t billboard rank \t artist \t song \n
+		song_info = line.strip().split('\t')
+		#print song_info[2],
+		#print '\t\t',
+		#print song_info[3]
+		song_set.append(Song(song_info[2], song_info[3]))
+
+for song in song_set:
+	print song.title, song.artist
