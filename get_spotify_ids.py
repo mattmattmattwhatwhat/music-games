6dedc67bbc8e9884da022a978673938ab0bea91e
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2014-07-07 18:45:48 -0400

    added method to find song match from search set

--- get_spotify_ids.py
+++ get_spotify_ids.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import string
 
 # I don't have a big song database and won't be able to github.io host a
 # spotify API key, so this will be a fragile way to get spotify ids in
@@ -9,6 +10,62 @@ class Song:
 	def __init__(self, artist, song_title):
 		self.artist = artist
 		self.title = song_title
+
+	def find_match_in_found_tracks(self, search_items):
+		# can't distinguish between multiple perfect matches
+		# picks the first it encounters
+
+		artist_as_list = self.artist.split()
+		title_as_list = self.title.split()
+
+		possible_matches = len(artist_as_list) + len(title_as_list)
+		best_match_count = 0
+		best_item = None
+
+		perfect_matches = []
+
+		for item in search_items:
+			track_name = item['name']
+			artists = item['artists']
+
+			artist_matches = 0
+			title_matches = 0
+
+			for track_chunk in title_as_list:
+				search_chunk = track_chunk.strip(string.punctuation).lower()
+				if track_name.lower().find(search_chunk) > -1:
+					title_matches += 1
+
+			for artist in artists:
+				for artist_chunk in artist_as_list:
+					search_chunk = artist_chunk.strip(string.punctuation).lower()
+
+					if artist['name'].lower().find(search_chunk) > -1:
+						artist_matches += 1
+
+			total_matches = artist_matches + title_matches
+
+			if total_matches == possible_matches:
+				perfect_matches.append(item)
+
+			if total_matches > best_match_count:
+				best_item = item
+				best_match_count = total_matches
+
+		if len(perfect_matches) == 1:
+			return perfect_matches[0]
+
+		elif len(perfect_matches) > 1:
+			for match in perfect_matches:
+				artist_name = match['artists'][0]['name'].lower()
+
+				if artist_name == self.artist.lower():
+					return match
+
+		return best_item
+
+
+
 
 def search_for_track(song_title):
 	base_search_url = "https://api.spotify.com/v1/search?q=track:"
@@ -23,22 +80,11 @@ song_set = []
 
 with open('song_list.txt') as f:
 	for line in f:
-		# lines in the form: year \t billboard rank \t artist \t song \n
 		song_info = line.strip().split('\t')
-		#print song_info[2],
-		#print '\t\t',
-		#print song_info[3]
 		song_set.append(Song(song_info[2], song_info[3]))
 
-for song in song_set:
-	print song.title, song.artist
+for song in song_set[:1]:
+	search_results = search_for_track(song.title)
+	song_results = search_results['tracks']['items']
 
-test_song = song_set[0]
-
-search_results = search_for_track(test_song.title)
-song_result_list = search_results['tracks']['items']
-
-for song in song_result_list:
-	print song['name']
-
-
+	print song.find_match_in_found_tracks(song_results)
