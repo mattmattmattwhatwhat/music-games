bc1b49d9d42069043002d35e63ffcf752ca40626
mattmattmattwhatwhat <matt.thompson0@gmail.com>
2014-07-07 16:30:42 -0400

    added spotify track search by song title

--- get_spotify_ids.py
+++ get_spotify_ids.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # I don't have a big song database and won't be able to github.io host a
 # spotify API key, so this will be a fragile way to get spotify ids in
@@ -8,6 +9,15 @@ class Song:
 	def __init__(self, artist, song_title):
 		self.artist = artist
 		self.title = song_title
+
+def search_for_track(song_title):
+	base_search_url = "https://api.spotify.com/v1/search?q=track:"
+	fixed_title = song_title.replace(' ', '%20')
+	search_url = base_search_url + fixed_title + '&type=track'
+
+	search_results = requests.get(search_url)
+
+	return json.loads(search_results.text)
 
 song_set = []
 
@@ -22,3 +32,13 @@ with open('song_list.txt') as f:
 
 for song in song_set:
 	print song.title, song.artist
+
+test_song = song_set[0]
+
+search_results = search_for_track(test_song.title)
+song_result_list = search_results['tracks']['items']
+
+for song in song_result_list:
+	print song['name']
+
+
